ef7385e4ff4ad3b44bda205d9ddde592720a7067
Анна <anna@MacBook-Air.local>
2023-05-04 16:15:56 -0400

    Edit backend port

--- reactapp/src/configs/config.js
+++ reactapp/src/configs/config.js
@@ -1,5 +1,5 @@
 
 export default {
-	API_URL: "http://127.0.0.1:8000/api/v1",
-	CRU_URL : "http://127.0.0.1:8000"
+	API_URL: "http://ec2-34-238-126-237.compute-1.amazonaws.com:8000/api/v1",
+	CRU_URL : "http://ec2-34-238-126-237.compute-1.amazonaws.com:8000"
 };
